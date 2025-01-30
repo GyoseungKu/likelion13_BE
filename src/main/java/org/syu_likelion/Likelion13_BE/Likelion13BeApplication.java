a57e842aacad8a5ec7d43a18e1edb8444c2816ba
Gyoseung Ku <ku@gyoseung.me>
2025-01-30 23:52:40 +0900

    Add Files

---
+++ src/main/java/org/syu_likelion/Likelion13_BE/Likelion13BeApplication.java
@@ -0,0 +1,14 @@
+package org.syu_likelion.Likelion13_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion13BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion13BeApplication.class, args);
+	}
+
+	//즐거운 개발 되세요! 히히
+}
---
+++ src/test/java/org/syu_likelion/Likelion13_BE/Likelion13BeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syu_likelion.Likelion13_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion13BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
