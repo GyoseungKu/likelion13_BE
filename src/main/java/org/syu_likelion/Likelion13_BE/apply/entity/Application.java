57cb83dfadb84b68f25ffaaba1bd2988afb25d0a
Gyoseung Ku <ku@gyoseung.me>
2025-02-13 16:00:46 +0900

    지원서 양식 변경, 이메일 전송 기능 추가

--- src/main/java/org/syu_likelion/Likelion13_BE/apply/entity/Application.java
+++ src/main/java/org/syu_likelion/Likelion13_BE/apply/entity/Application.java
@@ -41,56 +41,25 @@ public class Application {
     private Track track;
 
     // 트랙 지원 동기
-    @Column(name = "track_q")
-    private String trackQ;
+    @Column(name = "q1")
+    private String Q1;
 
-    // 백엔드 질문
-    @Column(name = "be_q1")
-    private String beQ1;
+    // 트랙별 질문 1
+    @Column(name = "q2")
+    private String Q2;
 
-    @Column(name = "be_q2")
-    private String beQ2;
+    // 트랙별 질문 2
+    @Column(name = "q3")
+    private String Q3;
 
-    @Column(name = "be_q3")
-    private String beQ3;
+    // 트랙별 질문 3
+    @Column(name = "q4")
+    private String Q4;
 
-    @Column(name = "be_q4")
-    private String beQ4;
+    // 포트폴리오 첨부
+    @Column(name = "q5")
+    private String Q5;
 
-    @Column(name = "be_q5")
-    private String beQ5;
-
-    // 프론트 질문
-    @Column(name = "fe_q1")
-    private String feQ1;
-
-    @Column(name = "fe_q2")
-    private String feQ2;
-
-    @Column(name = "fe_q3")
-    private String feQ3;
-
-    @Column(name = "fe_q4")
-    private String feQ4;
-
-    @Column(name = "fe_q5")
-    private String feQ5;
-
-    // 기디 질문
-    @Column(name = "pd_q1")
-    private String pdQ1;
-
-    @Column(name = "pd_q2")
-    private String pdQ2;
-
-    @Column(name = "pd_q3")
-    private String pdQ3;
-
-    @Column(name = "pd_q4")
-    private String pdQ4;
-
-    @Column(name = "pd_q5")
-    private String pdQ5;
 
     @Column(name = "created_at", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp createdAt;
@@ -103,6 +72,7 @@ public class Application {
         this.id = id;
     }
 
+    // 이름
     public String getName() {
         return name;
     }
@@ -111,6 +81,7 @@ public class Application {
         this.name = name;
     }
 
+    // 학번
     public String getStudentNum() {
         return studentNum;
     }
@@ -119,6 +90,7 @@ public class Application {
         this.studentNum = studentNum;
     }
 
+    // 전화번호
     public String getPhoneNum() {
         return phoneNum;
     }
@@ -127,6 +99,7 @@ public class Application {
         this.phoneNum = phoneNum;
     }
 
+    // 이메일
     public String getEmail() {
         return email;
     }
@@ -135,6 +108,7 @@ public class Application {
         this.email = email;
     }
 
+    // 노트북 보유 여부
     public boolean isLaptop() {
         return laptop;
     }
@@ -143,6 +117,7 @@ public class Application {
         this.laptop = laptop;
     }
 
+    // 세션 참여 여부
     public boolean isSession() {
         return session;
     }
@@ -151,6 +126,7 @@ public class Application {
         this.session = session;
     }
 
+    // 트랙 선택
     public Track getTrack() {
         return track;
     }
@@ -159,132 +135,49 @@ public class Application {
         this.track = track;
     }
 
-    public String getTrackQ() {
-        return trackQ;
+    // 트랙 지원 동기
+    public String getQ1() {
+        return Q1;
     }
 
-    public void setTrack_q(String trackQ) {
-        this.trackQ = trackQ;
+    public void setQ1(String Q1) {
+        this.Q1 = Q1;
     }
 
-    public String getBeQ1() {
-        return beQ1;
+    // 트랙별 질문 1
+    public String getQ2() {
+        return Q2;
     }
 
-    public void setBeQ1(String beQ1) {
-        this.beQ1 = beQ1;
+    public void setQ2(String Q2) {
+        this.Q2 = Q2;
     }
 
-    public String getBeQ2() {
-        return beQ2;
+    // 트랙별 질문 2
+    public String getQ3() {
+        return Q3;
     }
 
-    public void setBeQ2(String beQ2) {
-        this.beQ2 = beQ2;
+    public void setQ3(String Q3) {
+        this.Q3 = Q3;
     }
 
-    public String getBeQ3() {
-        return beQ3;
+    // 트랙별 질문 3
+    public String getQ4() {
+        return Q4;
     }
 
-    public void setBeQ3(String beQ3) {
-        this.beQ3 = beQ3;
+    public void setQ4(String Q4) {
+        this.Q4 = Q4;
     }
 
-    public String getBeQ4() {
-        return beQ4;
+    // 포트폴리오 첨부
+    public String getQ5() {
+        return Q5;
     }
 
-    public void setBeQ4(String beQ4) {
-        this.beQ4 = beQ4;
-    }
-
-    public String getBeQ5() {
-        return beQ5;
-    }
-
-    public void setBeQ5(String beQ5) {
-        this.beQ5 = beQ5;
-    }
-
-    public String getFeQ1() {
-        return feQ1;
-    }
-
-    public void setFeQ1(String feQ1) {
-        this.feQ1 = feQ1;
-    }
-
-    public String getFeQ2() {
-        return feQ2;
-    }
-
-    public void setFeQ2(String feQ2) {
-        this.feQ2 = feQ2;
-    }
-
-    public String getFeQ3() {
-        return feQ3;
-    }
-
-    public void setFeQ3(String feQ3) {
-        this.feQ3 = feQ3;
-    }
-
-    public String getFeQ4() {
-        return feQ4;
-    }
-
-    public void setFeQ4(String feQ4) {
-        this.feQ4 = feQ4;
-    }
-
-    public String getFeQ5() {
-        return feQ5;
-    }
-
-    public void setFeQ5(String feQ5) {
-        this.feQ5 = feQ5;
-    }
-
-    public String getPdQ1() {
-        return pdQ1;
-    }
-
-    public void setPdQ1(String pdQ1) {
-        this.pdQ1 = pdQ1;
-    }
-
-    public String getPdQ2() {
-        return pdQ2;
-    }
-
-    public void setPdQ2(String pdQ2) {
-        this.pdQ2 = pdQ2;
-    }
-
-    public String getPdQ3() {
-        return pdQ3;
-    }
-
-    public void setPdQ3(String pdQ3) {
-        this.pdQ3 = pdQ3;
-    }
-
-    public String getPdQ4() {
-        return pdQ4;
-    }
-
-    public void setPdQ4(String pdQ4) {
-        this.pdQ4 = pdQ4;
-    }
-
-    public String getPdQ5() {
-        return pdQ5;
-    }
-
-    public void setPdQ5(String pdQ5) {
-        this.pdQ5 = pdQ5;
+    public void setQ5(String Q5) {
+        this.Q5 = Q5;
     }
 
     public Timestamp getCreatedAt() {
--- src/main/java/org/syu_likelion/Likelion13_BE/apply/service/ApplicationService.java
+++ src/main/java/org/syu_likelion/Likelion13_BE/apply/service/ApplicationService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.syu_likelion.Likelion13_BE.apply.entity.Application;
 import org.syu_likelion.Likelion13_BE.apply.repository.ApplicationRepository;
+import org.syu_likelion.Likelion13_BE.email.EmailService;
 
 @Service
 public class ApplicationService {
@@ -11,7 +12,45 @@ public class ApplicationService {
     @Autowired
     private ApplicationRepository applicationRepository;
 
+    @Autowired
+    private EmailService emailService;
+
     public Application saveApplication(Application application) {
-        return applicationRepository.save(application);
+        Application savedApplication = applicationRepository.save(application);
+        sendConfirmationEmail(savedApplication);
+        return savedApplication;
+    }
+
+    private void sendConfirmationEmail(Application application) {
+        String to = application.getEmail();
+        String subject = "[멋쟁이사자처럼 삼육대학교] 지원 완료 안내";
+        String track = "";
+        switch (application.getTrack()) {
+            case BACKEND:
+                track = "백엔드";
+                break;
+            case FRONTEND:
+                track = "프론트엔드";
+                break;
+            case PLAN_DESIGN:
+                track = "기획/디자인";
+                break;
+        }
+        String text = "안녕하세요, " + application.getName() + "님. 지원이 완료되었습니다.\n\n"
+                + "-지원 내용-\n"
+                + "이름: " + application.getName() + "\n"
+                + "전화번호: " + application.getPhoneNum() + "\n"
+                + "이메일: " + application.getEmail() + "\n"
+                + "노트북 소지 여부: " + (application.isLaptop() ? "예" : "아니오") + "\n"
+                + "세션 참여 여부: " + (application.isSession() ? "예" : "아니오") + "\n"
+                + "선택한 트랙: " + track + "\n"
+                + "트랙 지원 동기: " + application.getQ1() + "\n"
+                + "트랙별 질문 1: " + application.getQ2() + "\n"
+                + "트랙별 질문 2: " + application.getQ3() + "\n"
+                + "트랙별 질문 3: " + application.getQ4() + "\n"
+                + "포트폴리오 첨부: " + application.getQ5() + "\n"
+                + "\n"
+                + "지원해주셔서 감사합니다.";
+        emailService.sendEmail(to, subject, text);
     }
 }
